f4182c6ecc6997318d7ef783173f15cfd64b9dfa
Doaa <doaaismaelm@gmail.com>
2019-07-20 17:40:01 +0200

    login page UI

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,18 +1,83 @@
-import React from 'react';
-const Login = () => {
-    return (
-    <React.Fragment>
-        <form action="">
-            <div className="form-group">
-                <label htmlFor="email"></label>
-                <input id="email" name="email" type="text" className="form-control"/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password"></label>
-                <input id="password" name="password" type="text" className="form-control" />
-            </div>
-        </form>
-    </React.Fragment>);
+import React, { Component } from 'react';
+import Joi from 'joi-browser';
+
+class Login extends Component {
+
+    state = {
+        account: { email: '', password: '' },
+        errors: { }, 
+        disableSubmitButton: false
+    }
+    schema = {
+        email: Joi.string().email().required().label('Email'),
+        password: Joi.string().required().label('Password')
+    }
+
+    render() {
+        const { account, errors, disableSubmitButton } = this.state; 
+        return (
+            <React.Fragment>
+                
+                <div className="container mt-2">
+                    <h1>Login</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input 
+                                id="email"
+                                name="email"
+                                type="text"
+                                className="form-control"
+                                value={account.email}
+                                onChange={this.handleChange}
+                                type="email" />
+                            {errors['email'] && <div className="alert alert-danger">{errors['email']}</div>}
+                        
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Passwrod</label>
+                            <input 
+                                id="password"
+                                name="password"
+                                type="text"
+                                className="form-control"
+                                value={account.password}
+                                onChange={this.handleChange}
+                                type="password"  />
+
+                        {errors['password'] && <div className="alert alert-danger">{errors['password']}</div>}
+
+                        </div>
+                        <button className="btn btn-primary" disabled={disableSubmitButton} >Login</button>
+                    </form>
+                </div>
+            </React.Fragment>
+        );
+    }
+
+    handleChange = ({target}) => {
+        let { account } = this.state;
+        account[target.name] = target.value;
+        this.setState({account, disableSubmitButton: false});
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.setState({ disableSubmitButton: true  });
+        let errors = this.validate();
+        this.setState({ errors: errors || {} });
+        if(errors) // Errors Exists
+            return;
+    }
+
+    validate = () => {
+        const result = Joi.validate(this.state.account, this.schema, {abortEarly: false});
+        console.log(result)
+        if(!result.error) return null;
+        const errors = {};
+        result.error.details.reduce((prev, cur) => (prev[cur.path[0]] = cur.message, prev) ,  errors);
+        return errors;
+    }
 }
 
 export default Login;
