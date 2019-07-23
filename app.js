019da8c376356ecfbd8af459710a0b360691a1b5
Doaa <doaaismaelm@gmail.com>
2019-07-23 16:44:34 +0200

    build for heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname+'/client/build')));
 
 // Bootstrap 
 require('./src/bootstrap/processErrors.js')();
@@ -22,6 +23,14 @@ require('./src/bootstrap/db.js')();
 
 // Routes
 require('./src/routes/index.js')(app);
+
+
+
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 
 module.exports = app;
